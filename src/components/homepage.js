31e940ca9ff74923eea9b4ea413be3ce2fdde1de
Shoblik <shoblik@yahoo.com>
2018-03-31 13:41:40 -0700

    landing page with centered title

---
+++ src/components/homepage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div className="homepage_bg">
+                <div className="filter">
+                    <div className='titleFilter'>
+                        <h1>Jessie Yount</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './components/app';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Homepage from './components/homepage';
+import './assets/css/app.css';
 
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+        <div>
+            <Switch>
+                <Route path="/" component={Homepage} />
+            </Switch>
+        </div>
+    </BrowserRouter>,
     document.getElementById('root')
 );
