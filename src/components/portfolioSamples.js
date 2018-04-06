9039dfe994ee2fce4a926b6dde48c30f72e2250c
Shoblik <shoblik@yahoo.com>
2018-04-06 14:35:11 -0700

    layout for portfolio section complete

--- src/components/portfolioSamples.js
+++ src/components/portfolioSamples.js
@@ -5,14 +5,20 @@ class PortfolioSamples extends Component {
     generateList() {
         let jsxArr = [];
         for (let i=0; i < 6; i++) {
-            let jsx = <div key={i} className='portfolioContainer'></div>
+            let jsx =
+                <div key={i} className='portfolioContainer'>
+                    <div className='btnContainer'>
+                        <button className='aboutBtn'>About</button>
+                        <button className='viewBtn'>View</button>
+                    </div>
+                </div>;
             jsxArr.push(jsx);
         }
         return jsxArr;
     }
     render() {
         return (
-            <div>
+            <div className='coverContainer'>
                 {this.generateList()}
             </div>
         )
