ade01ce07e4789923ab5e7fb0304a4a1d7473d1f
Shoblik <shoblik@yahoo.com>
2018-04-15 11:42:03 -0700

    changed the layout of the portfolio samples

--- src/components/portfolioSamples.js
+++ src/components/portfolioSamples.js
@@ -9,7 +9,8 @@ class PortfolioSamples extends Component {
                 <div key={i} className='portfolioContainer'>
                     <div className="portfolioFilter">
                         <div className='btnContainer'>
-                            <button className='aboutBtn'>About</button>
+                            <div className ="coverImgContainer"></div>
+                            <div className ="coverTextContainer"><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequuntur culpa dolorem harum ipsam laudantium minus nemo sequi tenetur. At cum cupiditate dolore eaque fugiat iste nobis pariatur vel, vero? Alias architecto consequatur ea, eligendi eveniet inventore modi quibusdam sed. Aliquam aspernatur itaque minus nesciunt, nobis pariatur praesentium reiciendis sequi sit sunt tempore unde. Ducimus.</div></div>
                             <button className='viewBtn'>View</button>
                         </div>
                     </div>
